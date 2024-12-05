f5981e5cecd356062626d31b8990064cd98af430
Shahab <shahabsafaei@protonmail.com>
2024-12-06 02:56:44 +0330

    Updated scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const navUl = document.querySelector('nav ul');
 
-    hamburger.addEventListener('click', () => {
-        navUl.classList.toggle('show');
-    });
+    if (hamburger && navUl) {
+        hamburger.addEventListener('click', () => {
+            navUl.classList.toggle('show');
+        });
+    } else {
+        console.error('Hamburger or nav element not found!');
+    }
 });
